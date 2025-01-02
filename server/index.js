7c35b95e8612f460cfef5d0c1a246f1165a82a09
JunaidArifFarooqui <arifjunaid25@gmail.com>
2025-01-02 11:00:12 +0500

    fix: vercel

--- server/index.js
+++ server/index.js
@@ -17,7 +17,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://pet-care-nyiz.vercel.app",
+    ],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
